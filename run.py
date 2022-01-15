a5bd96813b1bfde80bc3e4671b98a8d17ed4397f
Liang Peng <pengliang@piesat.cn>
2022-01-15 13:32:00 +0800

    Sat 15 Jan 2022 01:32:00 PM CST

--- run.py
+++ run.py
@@ -6,11 +6,13 @@ import os
 
 # move data to select era dir
 if True:  # {{{
-    infiles = glob.glob('/pub/ldx/era/src/201*_0.125x0.125_ldx')  # /pub/ldx/era/src/2017072206_0.125x0.125_ldx
+    infiles = glob.glob('/pub/ldx/era/src/201*_0.125x0.125_ldx')
+
     for infile in infiles:
         stime = re.search(r'(\d{10})', infile).group(1)+'0000'
         ctime = htt.str2time(stime)
-        outdir = '/run/media/ldx/My Passport/newJC/selec-era5/'+htt.time2str(ctime, 'yyyy')+'/'
+        outdir = '/run/media/ldx/My Passport/newJC/selec-era5/' + \
+            htt.time2str(ctime, 'yyyy')+'/'
 
         os.system('cp '+infile+' "'+outdir+'"')
 # }}}
