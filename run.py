3446b1e5f1124549a27bb201dcedf41b97b18cfe
Liang Peng <pengliang@piesat.cn>
2022-01-17 13:12:57 +0800

    Mon 17 Jan 2022 01:12:57 PM CST

--- run.py
+++ run.py
@@ -2,6 +2,9 @@ import glob
 import re
 import time_htht as htt
 import os
+import h5py as h5
+# import netCDF4 as nc
+import numpy as np
 
 
 # move data to select era dir
@@ -18,7 +21,7 @@ if False:  # {{{
 # }}}
 
 # change the name
-if True:  # {{{
+if False:  # {{{
     for year in range(2015, 2020):
         indir = '/home/ldx/mp/newJC/selec-era5/'+str(year)+'/'
         infiles = glob.glob(indir+'*_0000_0.125x0.125.ldx.nc')
@@ -38,4 +41,33 @@ if True:  # {{{
                 cmd = 'mv '+infile+' '+outfile
                 print(cmd)
                 os.system(cmd)
+# }}}
+
+# calculate the yearly mean
+if True:  # {{{
+    for year in range(2015, 2020):
+        indir = '/home/ldx/mp/newJC/selec-era5/'+str(year)+'/'
+        ty = 0
+        spdy = 0
+        uy = 0
+        vy = 0
+        for doy in range(1, 366):
+            for hour in [0, 6, 12, 18]:
+                ctime = htt.vec2time(year, 1, doy, hour, 0, 0)
+                infile = indir+htt.time2str(ctime, 'yyyy_mm_dd_HHMM') +\
+                    '_2.125x0.125.ldx.nc'
+                f = h5.File(infile, 'r')
+                t = f['t'][:]
+                u = f['u'][:]
+                v = f['v'][:]
+                f.close()
+
+                spd = np.sqrt(u**2+v**2)
+
+                ty += t
+                uy += u
+                vy += v
+                spdy += spd
+        
+
 # }}}
