a098a70c2e643c9d86d857b7f1c99d249e4f6c99
Liang Peng <pengliang@piesat.cn>
2022-01-17 12:51:18 +0800

    Mon 17 Jan 2022 12:51:18 PM CST

--- run.py
+++ run.py
@@ -5,14 +5,35 @@ import os
 
 
 # move data to select era dir
-if True:  # {{{
+if False:  # {{{
     infiles = glob.glob('/pub/ldx/era/src/201*_0.125x0.125_ldx')
 
     for infile in infiles:
         stime = re.search(r'(\d{10})', infile).group(1)+'0000'
         ctime = htt.str2time(stime)
-        outdir = '/run/media/ldx/My Passport/newJC/selec-era5/' + \
+        outdir = '/home/ldx/mp/newJC/selec-era5/' + \
             htt.time2str(ctime, 'yyyy')+'/'
 
         os.system('cp '+infile+' "'+outdir+'"')
+# }}}
+
+# set the soft links for the new data
+if True:  # {{{
+    for year in range(2015, 2020):
+        indir = '/home/ldx/mp/newJC/selec-era5/'+str(year)+'/'
+        infiles = glob.glob(indir+'*_0000_0.125x0.125.ldx.nc')
+        os.chdir(indir)
+
+        for infile00 in infiles:
+            stime00 = re.search(r'(\d{4}_\d\d_\d\d_\d\d\d\d)', infile).group(1)
+            stime00 = re.sub('_', '', stime00)
+            ctime00 = htt.str2time(stime00)
+            for hour in [6, 12, 18]:
+                ctime = ctime00 + hour*3600
+                infile = indir+htt.time2str(ctime, 'yyyymmddHH') +\
+                    '_0.125x0.125_ldx'
+                outfile = indir+htt.time2str(ctime, 'yyyy_mm_dd_HHMM') +\
+                    '_2.125x0.125.ldx.nc'
+                cmd = 'mv '+infile+' '+outfile
+                print(cmd)
 # }}}
